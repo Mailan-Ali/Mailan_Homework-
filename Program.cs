55747a1aed53b26de8eb08da476d7964dd2a351a
Mailan-Ali <77187891+Mailan-Ali@users.noreply.github.com>
2021-01-13 18:47:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Age");
+            String ob = Console.In.ReadLine();
+            int age = int.Parse(ob);
+            int birth = 2020 - age;
+            Console.WriteLine("Your Birthday : "+birth);
+        }
+    }
+}
